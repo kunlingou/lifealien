373bcb0fb85555f47efc6a53976e41199b0bc910
kunlingou <kunlingou@foxmail.com>
2019-05-26 10:45:24 +0800

    1

---
+++ lifealien_backend/lifealien.spring/src/main/java/com/kunlinr/lifealien/spring/jdbc/JDBCTest.java
@@ -0,0 +1,8 @@
+package com.kunlinr.lifealien.spring.jdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JDBCTest {
+	ApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+}
