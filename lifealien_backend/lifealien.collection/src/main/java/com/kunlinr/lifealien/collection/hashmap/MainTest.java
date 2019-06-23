52d82c7fe85126649b66dd6175e016f05e8957d5
kunlingou <kunlingou@foxmail.com>
2019-06-23 09:59:07 +0800

    hashmap

--- lifealien_backend/lifealien.collection/src/main/java/com/kunlinr/lifealien/collection/hashmap/MainTest.java
+++ lifealien_backend/lifealien.collection/src/main/java/com/kunlinr/lifealien/collection/hashmap/MainTest.java
@@ -41,11 +41,41 @@ public class MainTest {
 //		concurrentHashMap.put(null, "1");
 //		concurrentHashMap.put(null, null);
 		
-		syncMapPerformance(hashtable);
-		syncMapPerformance(synchronizedMap);
-		syncMapPerformance(concurrentHashMap);
-		syncMapPerformance(hashMap);
+//		syncMapPerformance(hashtable);
+//		syncMapPerformance(synchronizedMap);
+//		syncMapPerformance(concurrentHashMap);
+//		syncMapPerformance(hashMap);
+		
+//		float v = 0.0f/0.0f;
+//		System.out.println(Float.isNaN(v));
+//		System.out.println(v != v);
+		
+		HashMap<String, Object> hashMap2 = new HashMap<String, Object>(1 << 4);
+		
+		for(int i=0;i<13;i++) {
+			if(i==12) {
+				System.out.println();
+			}
+			hashMap2.put(String.valueOf(i), 0);
+		}
+		
+		int tableSizeFor = tableSizeFor(65);
+		System.out.println(tableSizeFor);
 	}
+	
+	/**
+	* 获取不小于当前数的最小的2^k的值.
+	* 例如：31->32,65->128
+	*/
+	static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return n+1;
+    }
 	
 	/**
 	 * 测试多线程下各种map的性能
